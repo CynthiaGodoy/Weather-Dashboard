c2adbc4e0446331b329840aa75aae09764b04d2b
Cynthia Godoy <cynthia@ck-interiors.com>
2022-04-02 22:33:39 -0600

    a little more js

--- script.js
+++ script.js
@@ -38,7 +38,48 @@ searchCityButton.on("click", function(event) {
 
 var searchValue = searchCityInput.val().trim();
 
-currentConditionRequest (searchValue)
+currentConditionsRequest(searchValue)
 searchHistory(searchValue);
 searchCityInput.val("");
 });
+
+clearHistoryButton.on("click", function() {
+    cityList = [];
+    listArray();
+    $(this).addClass("hide");
+});
+
+searchHistoryList.on("click", "li.city-btn", function(event){
+    var value = $(this).data("value");
+    currentConditionsRequest(value);
+    searchHistory(value);
+});
+
+function currentConditionsRequest(searchValue){
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=imperial&appid=" + APIkey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }) .then(function(response) {
+        console.log(response);
+        currentCity.text(response.name);
+        currentCity.append("<small class='text-muted' id='current-date'>");
+        $("#current-date").text("(" + currentDate + ")");
+        currentCity.append("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='" + response.weather[0].main + "' />" )
+        currentTemp.text(response.main.temp);
+        currentTemp.apped("$deg;F");
+        currentHumidity.text(reposnse.main.humidity + "%");
+        currentWindSpeed.text(response.wind.speed + "MPH");
+
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+
+        var UVurl = "https://api.openweathermap.org/data/2.5/uvi?&lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
+
+        $.ajax({
+            url: UVurl,
+            
+        })
+    })
+}
+
