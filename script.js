d57591f74bc930771c80009986ee230f10b8f93d
Cynthia Godoy <cynthia@ck-interiors.com>
2022-04-02 21:22:07 -0600

    working on js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var UVindex = $("#uv-index");
 //JQUERY VARIABLE FOR 5-DAY FORCAST
 var weatherContent = $("#weather-content");
 
-var APIkey = "2e985842de3879dadd49c9909fe5946c" //ALLOWS ACCESS TO API - SIGN IN TO APP AND CREATE ACCOUNT GET UNIQUE CODE
+var APIkey = "2e985842de3879dadd49c9909fe5946c"; //ALLOWS ACCESS TO API - SIGN IN TO APP AND CREATE ACCOUNT GET UNIQUE CODE
 
 var cityList = []; //ACCESS THE DATA
 
@@ -33,9 +33,12 @@ searchHistory(searchValue);
 searchCityInput.val("");
 });
 
-searchCityButton.on("click", function(event)) {
-    event.preventDefault() {
+searchCityButton.on("click", function(event) {
+    event.preventDefault(); 
 
-    }
-}
+var searchValue = searchCityInput.val().trim();
 
+currentConditionRequest (searchValue)
+searchHistory(searchValue);
+searchCityInput.val("");
+});
